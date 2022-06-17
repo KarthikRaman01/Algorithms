f2c5de46b1a17094c7ef51ac9fdc45c61049a7c0
Karthik Raman <kartikram01@gmail.com>
2022-06-17 17:27:46 -0400

    some more additions to permutation

--- src/com/recursion/strings/subsetandsubsequence/Permutation.java
+++ src/com/recursion/strings/subsetandsubsequence/Permutation.java
@@ -1,10 +1,14 @@
 package com.recursion.strings.subsetandsubsequence;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		permutation("", "abc");
+		System.out.println(permutationList("", "abc"));
+		System.out.println(permutationCount("", "abc"));
 	}
 
 	public static void permutation(String processed, String unProcessed) {
@@ -19,6 +23,39 @@ public class Permutation {
 			permutation(p, unProcessed.substring(1));
 		}
 
+	}
+
+	public static List<String> permutationList(String processed, String unProcessed) {
+		if (unProcessed.isEmpty()) {
+			List<String> output = new ArrayList<String>();
+			output.add(processed);
+			return output;
+		}
+
+		Character ch = unProcessed.charAt(0);
+		List<String> output = new ArrayList<String>();
+		for (int i = 0; i < processed.length() + 1; i++) {
+			String p = processed.substring(0, i) + ch.toString() + processed.substring(i);
+			List<String> returnedOutput = permutationList(p, unProcessed.substring(1));
+			output.addAll(returnedOutput);
+		}
+
+		return output;
+	}
+
+	public static Integer permutationCount(String processed, String unProcessed) {
+		if (unProcessed.isEmpty()) {
+			return 1;
+		}
+
+		Character ch = unProcessed.charAt(0);
+		Integer count = 0;
+		for (int i = 0; i < processed.length() + 1; i++) {
+			String p = processed.substring(0, i) + ch.toString() + processed.substring(i);
+			count = count + permutationCount(p, unProcessed.substring(1));
+		}
+
+		return count;
 	}
 
 }
